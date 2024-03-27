a9a0ce8b240c56417f300484518bcb16096fbf83
Alican Tarım <alicantarim@hotmail.com>
2024-03-27 22:34:42 +0300

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Alican TARIM on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator App/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Calculator App
+//
+//  Created by Alican TARIM on 24.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
